0118f0921f2780cb4185e7b48a615dc382917319
Filip <f.nikolin997@gmail.com>
2026-09-07 17:44:10 +0200

    testing commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import http from "node:http";
 
 const server = http.createServer((req, res) => {
   console.log(req.method, JSON.stringify(req.url));
-  res.end("hello");
   console.log("test");
+  console.log("test");
+  console.log("test");
+  res.end("hello");
 });
 
 server.listen(3000, () => console.log("listen on :3000"));
