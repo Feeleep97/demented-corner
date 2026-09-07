6bfed841da0aa225848aca394af591d34c4df1e0
Filip <meplano.dev@zik-tech.com>
2026-09-07 17:32:30 +0200

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import http from "node:http";
 const server = http.createServer((req, res) => {
   console.log(req.method, JSON.stringify(req.url));
   res.end("hello");
+  console.log("test");
 });
 
 server.listen(3000, () => console.log("listen on :3000"));
